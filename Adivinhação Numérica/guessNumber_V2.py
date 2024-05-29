156455ee24dae4f3b7afa0094d2f5624468c21a0
Pedro Augusto <apedro.rp@gmail.com>
2024-05-29 18:59:46 -0300

    fix: variable names

--- Adivinhação Numérica/guessNumber_V2.py
+++ Adivinhação Numérica/guessNumber_V2.py
@@ -15,21 +15,21 @@ def main():
     print('2. Médio (0-100)')
     print('3. Difícil (0-200)')
 
-nivel = input('Digite o número do nível desejado: ')
+userChoiceDifficult = input('Digite o número do nível desejado: ')
 
 
-if nivel in difficultyLevel:
-    choiceNumber = difficultyLevel[nivel]
+if userChoiceDifficult in difficultyLevel:
+    levelGame = difficultyLevel[userChoiceDifficult]
 else:
     print('Erro: nível inválido. Por favor, execute novamente e escolha um nível válido!')
     quit()
 
-randomNumber = random.randint(0, choiceNumber)
+randomNumber = random.randint(0, levelGame)
 
-nChoices = 0
-previous_guesses = []
+nAttempts = 0
+previousGuesses = []
 
-while nChoices < maxAttempts:
+while nAttempts < maxAttempts:
     answerUser = input('Adivinhe o número: ')
     
     if answerUser.isdigit():
@@ -38,35 +38,35 @@ while nChoices < maxAttempts:
         print('Erro: valor informado não é numérico. Favor informar um número')
         continue
 
-    nChoices += 1
-    previous_guesses.append(answerUser)
+    nAttempts += 1
+    previousGuesses.append(answerUser)
 
     if answerUser == randomNumber:
-        print(f'Parabéns! Você acertou o número {randomNumber} em {nChoices} tentativas!')
+        print(f'Parabéns! Você acertou o número {randomNumber} em {nAttempts} tentativas!')
         break
 
     diff = abs(answerUser - randomNumber)
-    percentage_diff = (diff / randomNumber) * 100  # Porcentagem de diferença
+    percentageDiff = (diff / randomNumber) * 100  # Porcentagem de diferença
 
     if answerUser > randomNumber:
-            if percentage_diff > 50:
+            if percentageDiff > 50:
                 print(f'Não foi desta vez! O número procurado é muito menor do que {answerUser}.')
-            elif percentage_diff > 20:
+            elif percentageDiff > 20:
                 print(f'Não foi desta vez! O número procurado é menor do que {answerUser}, mas não muito.')
             else:
                 print(f'Não foi desta vez! O número procurado é um pouco menor do que {answerUser}.')
     else:  # answerUser < randomNumber
-            if percentage_diff > 50:
+            if percentageDiff > 50:
                 print(f'Não foi desta vez! O número procurado é muito maior do que {answerUser}.')
-            elif percentage_diff > 20:
+            elif percentageDiff > 20:
                 print(f'Não foi desta vez! O número procurado é maior do que {answerUser}, mas não muito.')
             else:
                 print(f'Não foi desta vez! O número procurado é um pouco maior do que {answerUser}.')
     
     print(f"N° de tentativas: {nChoices}")
-    print(f"Palpites anteriores: {previous_guesses}")
+    print(f"Tentativas anteriores: {previousGuesses}")
 
-if nChoices == maxAttempts and answerUser != randomNumber:
+if nAttempts == maxAttempts and answerUser != randomNumber:
     print(f'Você atingiu o número máximo de tentativas ({maxAttempts}). O número correto era {randomNumber}.')
 
 if __name__ == '__main__':
