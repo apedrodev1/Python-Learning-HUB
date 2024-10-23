8c1fb69ca4640ba0f8ea0bf7a9824d399a9e12c9
Pedro <apedro.rp@gmail.com>
2024-10-23 12:40:32 -0300

    initial commit

---
+++ Média aluno OOP/media.py
@@ -0,0 +1,52 @@
+class Student:
+     def __init__ (self, name):
+            self.name = name
+            self.marks = []
+            self.final_mark = 0
+            self.condition= ''
+
+
+            def fillMarks (self):
+                try:
+                    qtd_marks = int(input('Quantas notas deseja inserir?'))
+                    for _ in range (qtd_marks):
+                        while True:
+                            try:
+                                nota = float(input(f'Digite a nota de {self.name}:'))
+                                self.marks.append(nota)
+                                break
+                            except ValueError:
+                                print("Por favor, insira um número válido.")
+                    self.calcMedia()
+                except ValueError:
+                    print("Por favor, insira um número válido.")
+
+
+            def calcMedia (self):
+                    if self.marks:
+                        self.finalMark = sum(self.marks) / len(self.marks) 
+
+                        if self.finalMark >= 7:
+                            self.condition = 'Aprovado'
+                        else:
+                            self.condition = 'Reprovado'
+
+                        return self.condition 
+                    else: 
+                        print('Nenhuma nota foi inserida.')
+                        return None
+
+
+while True:
+    aluno = Student(str(input('Digite o nome do Aluno: ')))
+    aluno.fillMarks()
+    aluno.calcMedia()
+    
+    print(f'Notas de {aluno.name}: {aluno.marks}')
+    print(f'Média: {aluno.finalMark:.2f}')
+    print(f'Situação: {aluno.condition}')
+
+    continuar = input('Deseja adicionar outro aluno? (S/N): ').lower()
+    if continuar == 'n':
+        print('Encerrando o programa...')
+        break
