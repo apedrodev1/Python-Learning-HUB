540ee12410b98c7a9653436486beaf6d688e67f8
Pedro <apedro.rp@gmail.com>
2024-10-24 18:15:13 -0300

    feat: def calculate_weighted_media

--- Média aluno OOP/functions/function.py
+++ Média aluno OOP/functions/function.py
@@ -2,3 +2,23 @@ import os
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def calculate_media(marks):
+    if marks:
+        return sum(marks) / len(marks)
+    else:
+        print('Nenhuma nota foi inserida.')
+        return 0
+    
+
+
+# def calculate_weighted_media(marks, weigthted_marks):
+#     if marks:
+
+    #formula media ponderada, recebendo os pesos como parametro, setar na class 
+#         print('Nenhuma nota foi inserida.')
+#         return 0
+#     else: 
+
+def check_condition(final_mark):
+    return 'Aprovado' if final_mark >= 7 else 'Reprovado'
