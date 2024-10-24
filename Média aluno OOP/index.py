380270ab42696dc385452cd889f4136c89bfc68d
Pedro <apedro.rp@gmail.com>
2024-10-24 17:27:05 -0300

    feat: index.py refactoring code

---
+++ Média aluno OOP/index.py
@@ -0,0 +1,28 @@
+from classes.classStudent import Student
+from functions.function import clear_screen
+
+while True:
+         
+    clear_screen()
+    
+    aluno = Student(str(input('Digite o nome do Aluno: ')))
+    aluno.fillMarks()
+
+    print(f'Boletim de {aluno.name}')
+    print('#############################')
+    print(f'{"Notas":<10} | {"Valores":>10}')
+    print('-----------------------------')
+
+    for i, nota in enumerate(aluno.marks, 1):
+        print(f'Nota {i:<7} | {nota:>10.2f}')
+
+    print('-----------------------------')
+    print(f'{"Média":<10} | {aluno.final_mark:>10.2f}')
+    print(f'{"Situação":<10} | {aluno.condition:>10}')
+
+    aluno.marks.clear()
+
+    continuar = input('Deseja adicionar outro aluno? (S/N): ').lower()
+    if continuar == 'n':
+        print('Encerrando o programa...')
+        break
