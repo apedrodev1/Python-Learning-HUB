815546caaa4b72aa350843bdf3f6ded52f374938
Pedro <apedro.rp@gmail.com>
2024-10-28 14:04:01 -0300

    refactor: new functions

--- Média aluno OOP/classes/classStudent.py
+++ Média aluno OOP/classes/classStudent.py
@@ -1,5 +1,3 @@
-from functions.function import calculate_media_arithmetic, check_condition
-
 class Student:
     def __init__(self, name):
         self.name = name
@@ -7,19 +5,22 @@ class Student:
         self.final_mark = 0
         self.condition = ''
 
-    # PODE SER FUNCTION STANDALONE 
-    def fill_marks(self):
-        try:
-            qtd_marks = int(input('Quantas notas deseja inserir? ')) #letra entra tbm, rever 
-            for i in range(1, qtd_marks + 1):
-                while True:
-                    try:
-                        nota = float(input(f'Digite a nota {i} de {self.name}: ')) #valores negativos entram, criar verificacao 
-                        self.marks.append(nota)
-                        break
-                    except ValueError:
-                        print("Por favor, insira um número válido.")
-            self.final_mark = calculate_media_arithmetic(self.marks)
-            self.condition = check_condition(self.final_mark)
-        except ValueError:
-            print("Por favor, insira um número válido.")
+    def calculate_media_arithmetic(marks):
+        if marks:
+            return sum(marks) / len(marks)
+        else:
+            print('Nenhuma nota foi inserida.')
+        return 0
+    
+
+    # def calculate_weighted_media(marks, weigthted_marks):
+#     if marks:
+
+    #formula media ponderada, recebendo os pesos como parametro, setar na class 
+#         print('Nenhuma nota foi inserida.')
+#         return 0
+#     else: 
+
+    
+    def check_condition(final_mark):
+        return 'Aprovado' if final_mark >= 7 else 'Reprovado'
--- Média aluno OOP/functions/function.py
+++ Média aluno OOP/functions/function.py
@@ -1,3 +1,4 @@
+from classes.classStudent import calculate_media_arithmetic, check_condition
 import os
 
 def clear_screen():
@@ -19,24 +20,3 @@ def fill_marks(self):
         except ValueError:
             print("Por favor, insira um número válido.")
             
-# MPODE SER MÉTODO NA CLASSE STUDENT
-def calculate_media_arithmetic(marks):
-    if marks:
-        return sum(marks) / len(marks)
-    else:
-        print('Nenhuma nota foi inserida.')
-        return 0
-    
-
-
-# def calculate_weighted_media(marks, weigthted_marks):
-#     if marks:
-
-    #formula media ponderada, recebendo os pesos como parametro, setar na class 
-#         print('Nenhuma nota foi inserida.')
-#         return 0
-#     else: 
-
-# MPODE SER MÉTODO NA CLASSE STUDENT
-def check_condition(final_mark):
-    return 'Aprovado' if final_mark >= 7 else 'Reprovado'
