9bab278d874c7bf757789a8d511ba9c285ebb8bf
Pedro <apedro.rp@gmail.com>
2024-10-28 12:48:18 -0300

    refactor: changing file of the function fill_marks

--- Média aluno OOP/functions/function.py
+++ Média aluno OOP/functions/function.py
@@ -3,6 +3,22 @@ import os
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def fill_marks(self):
+        try:
+            qtd_marks = int(input('Quantas notas deseja inserir? ')) #letra entra tbm, rever 
+            for i in range(1, qtd_marks + 1):
+                while True:
+                    try:
+                        nota = float(input(f'Digite a nota {i} de {self.name}: ')) #valores negativos entram, criar verificacao 
+                        self.marks.append(nota)
+                        break
+                    except ValueError:
+                        print("Por favor, insira um número válido.")
+            self.final_mark = calculate_media_arithmetic(self.marks)
+            self.condition = check_condition(self.final_mark)
+        except ValueError:
+            print("Por favor, insira um número válido.")
+            
 # MPODE SER MÉTODO NA CLASSE STUDENT
 def calculate_media_arithmetic(marks):
     if marks:
