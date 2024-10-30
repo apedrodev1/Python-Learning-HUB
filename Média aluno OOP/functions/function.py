e2ad08a118a1ab8e151348b5ba12789bd1ffc775
Pedro <apedro.rp@gmail.com>
2024-10-30 15:12:13 -0300

    refactor: def fill_marks input validation

--- Média aluno OOP/functions/function.py
+++ Média aluno OOP/functions/function.py
@@ -1,21 +1,35 @@
-
 import os
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def fill_marks(self):
-    try:
-        qtd_marks = int(input('Quantas notas deseja inserir? '))
-        for i in range(1, qtd_marks + 1):
-            while True:
-                try:
-                    nota = float(input(f'Digite a nota {i} de {self.name}: '))
+    while True:
+        try:
+            qtd_marks = int(input('Quantas notas deseja inserir? '))
+            if qtd_marks > 0:
+                break
+            else: 
+                print('Por favor, insira um número inteiro positivo.')
+
+        except ValueError: 
+            print ('Por favor, insira um número válido.')
+    
+    for i in range(1, qtd_marks + 1):
+        while True: 
+            try:
+                nota = float(input(f'Digite a nota {i} de {self.name}: '))
+                if 0 <= nota <= 10:
                     self.marks.append(nota)
                     break
-                except ValueError:
-                    print("Por favor, insira um número válido.")
-        self.final_mark = self.calculate_media_arithmetic()  
-        self.condition = self.check_condition()  
-    except ValueError:
-        print("Por favor, insira um número válido.")
+
+                else:
+                     print("Por favor, insira um número entre 0 e 10.")
+
+            except ValueError:
+                print("Por favor, insira um número válido.")
+
+
+
+    self.final_mark = self.calculate_media_arithmetic()  
+    self.condition = self.check_condition()
