a78f491418b9812670a9a4e5b201d9d6072b155d
Pedro <apedro.rp@gmail.com>
2024-11-01 17:32:17 -0300

    feat: def calculate_weighted_media

--- Média aluno OOP/classes/classStudent.py
+++ Média aluno OOP/classes/classStudent.py
@@ -3,25 +3,28 @@ class Student:
         self.name = name
         self.passing_mark = passing_mark
         self.marks = []
+        self.weighted_marks = []
         self.final_mark = 0
         self.condition = ''
 
     def calculate_media_arithmetic(self):
         if self.marks:
-            return sum(self.marks) / len(self.marks)
+            return sum (self.marks * self.weigthted_marks) / len(self.marks)
+
+    
+        print('Nenhuma nota foi inserida.')
+        return 0 
+    
+
+    def calculate_weighted_media(self):
+        if self.marks and self.weighted_marks:
+            total = sum (m * w for m, w in zip(self.marks, self.weighted_marks))
+
+            return total / sum (self.weighted_marks)
         else:
             print('Nenhuma nota foi inserida.')
         return 0
-    
+        
 
-    # def calculate_weighted_media(marks, weigthted_marks):
-#     if marks:
-
-    #formula media ponderada, recebendo os pesos como parametro, setar na class 
-#         print('Nenhuma nota foi inserida.')
-#         return 0
-#     else: 
-
-    
     def check_condition(self):
         return 'Aprovado' if self.final_mark >= self.passing_mark else 'Reprovado'
