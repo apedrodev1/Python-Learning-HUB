33f76e1d72ecef8ae16dce6de50eecf12b8e32b1
Pedro <apedro.rp@gmail.com>
2024-11-02 20:59:14 -0300

    refactor: code language

--- Média aluno OOP/functions/function.py
+++ Média aluno OOP/functions/function.py
@@ -6,37 +6,40 @@ def clear_screen():
 def fill_marks(student):
     while True:
         try:
-            qtd_marks = int(input('Quantas notas deseja inserir? '))
-            if qtd_marks > 0:
+            num_grades = int(input('How many grades would you like to enter? '))
+            if num_grades > 0:
                 break
-            else:
-                print('Por favor, insira um número inteiro positivo.')
-        except ValueError:
-            print('Por favor, insira um número válido.')
+            else: 
+                print('Please enter a positive integer.')
 
-    for i in range(1, qtd_marks + 1):
-        while True:
+        except ValueError: 
+            print('Please enter a valid number.')
+    
+    for i in range(1, num_grades + 1):
+        while True: 
             try:
-                grade = float(input(f'Digite a nota {i} de {student.name}: '))
+                grade = float(input(f'Enter grade {i} for {student.name}: '))
                 if 0 <= grade <= 10:
                     student.marks.append(grade)
                     break
-                else:
-                    print("Por favor, insira um número entre 0 e 10.")
-            except ValueError:
-                print("Por favor, insira um número válido.")
 
-        # Se média ponderada for selecionada, pergunte o peso
+                else:
+                    print("Please enter a number between 0 and 10.")
+
+            except ValueError:
+                print("Please enter a valid number.")
+
+        # If weighted mean is selected, ask for weight
         if student.is_weighted:
             while True:
                 try:
-                    weight = float(input(f'Digite o peso para a nota {i}: '))
+                    weight = float(input(f'Enter the weight for grade {i}: '))
                     student.weighted_marks.append(weight)
                     break
                 except ValueError:
-                    print("Por favor, insira um número válido para o peso.")
+                    print("Please enter a valid number for the weight.")
 
-    # Decide qual média calcular com base no tipo escolhido
-    student.final_mark = (student.calculate_weighted_media() if student.is_weighted 
-                          else student.calculate_media_arithmetic())
+    # Decide which average to calculate based on the chosen type
+    student.final_mark = (student.calculate_weighted_mean() if student.is_weighted 
+                          else student.calculate_arithmetic_mean())
     student.condition = student.check_condition()
