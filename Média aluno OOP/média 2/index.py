12e384ea3b4009031054e100b695656dd65d6483
Pedro <apedro.rp@gmail.com>
2025-04-23 16:18:30 -0300

    feat: laco de continuidade

--- Média aluno OOP/média 2/index.py
+++ Média aluno OOP/média 2/index.py
@@ -1,35 +1,24 @@
-#modulos
 from src.functions.parameters import get_main_parameters
 from src.functions.main_function import process_students
-#from src.functions.exporter import export_data
-
 
 print("🎓 Bem-vindo ao Sistema de Cálculo de Médias 🎓\n")
 
 def main():
-    
-    students_quantity, way_to_calculate, passing_grade, weights = get_main_parameters() ##crashando ao passar argumentos
-    students_list = process_students(students_quantity, way_to_calculate, passing_grade, weights)
-    
-#funcao export_data export_data(students_list)
-#funcao enviar para o email 
-#funcao imprimir na tela
+    while True:
+            students_quantity, way_to_calculate, passing_grade, weights = get_main_parameters() 
+            students_list = process_students(students_quantity, way_to_calculate, passing_grade, weights)
+        # Futuras funcionalidades
+        # export_data(students_list)
+        # enviar para email
+        # imprimir resultados na tela
+        # editar notas e alunos via ID
 
+            print(students_list)
 
-
+            retry = input("\n🔁 Deseja rodar o programa novamente? (s/n): ").lower()
+            if retry != 's':
+                print("\n👋 Programa finalizado. Até a próxima!")
+                break
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-(input(print('Deseja rodar de novo')))
-
-#if no mensagem de fim do programa  
-
-
