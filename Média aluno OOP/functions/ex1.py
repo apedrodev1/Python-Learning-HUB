4855c57d8614d7ab91544395aa6ba85f097ff0bc
Pedro <apedro.rp@gmail.com>
2025-04-25 17:16:33 -0300

    deleted files

--- Média aluno OOP/functions/ex1.py
+++
@@ -1,8 +0,0 @@
-produtos = {}
-
-print('Leitura de dados: ')
-
-cod = input('Digite o codigo do produto: ').strp()
-price = float(input(f'Digite o preco do produto {cod}: R$ '))
-produto = {'Codigo': cod, 'Valor': price}
-produtos = produto
