93d8109b87b6bf3388ccca3921e19e3ae30c63e0
Pedro <apedro.rp@gmail.com>
2025-04-25 16:14:24 -0300

    coments and new features

--- Média aluno OOP/média 2/index.py
+++ Média aluno OOP/média 2/index.py
@@ -2,6 +2,7 @@
 
 from src.functions.parameters import get_main_parameters
 from src.functions.main_function import process_students
+#from.src.functions.show_students import display_students
 from src.functions.loop_control import ask_to_retry
 
 
@@ -11,13 +12,18 @@ def main():
     while True:
             students_quantity, way_to_calculate, passing_grade, weights = get_main_parameters() 
             students_list = process_students(students_quantity, way_to_calculate, passing_grade, weights)
+            #display_students(students_list)
+        
+        
         # Futuras funcionalidades
+
+        # editar notas e alunos via ID
         # export_data(students_list)
         # enviar para email
-        # imprimir resultados na tela
-        # editar notas e alunos via ID
+        
+        
 
-            print(students_list)
+            
 
             retry = input("\n🔁 Deseja rodar o programa novamente? (s/n): ").lower() #usar a funcao
             if retry != 's':
