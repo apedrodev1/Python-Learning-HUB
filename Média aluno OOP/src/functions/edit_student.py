032ef903de2accf6a3f638b9ab60d1bfaff0b5da
Pedro <apedro.rp@gmail.com>
2025-04-26 14:30:37 -0300

    documents: setting PEP 257

--- Média aluno OOP/src/functions/edit_student.py
+++ Média aluno OOP/src/functions/edit_student.py
@@ -1,2 +1,11 @@
 def edit_student_data(student_list):
+    '''
+    Allows the user to edit the student_list if any incorrect data was entered.
+
+    Args:
+        student_list (list): List containing all Student instances.
+
+    Returns:
+        None
+    '''
     return
