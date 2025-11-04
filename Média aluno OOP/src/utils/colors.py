473b6521e97711e9009cc74a79f775f4d9606f48
Pedro Rocha <apedro.rp@gmail.com>
2025-11-04 10:25:46 -0300

    feat: PEP 257

--- Média aluno OOP/src/utils/colors.py
+++ Média aluno OOP/src/utils/colors.py
@@ -1,3 +1,10 @@
+"""
+Defines ANSI escape code constants for terminal colors.
+
+These constants can be imported by other modules (e.g., formatters)
+to add color to console output, enhancing readability.
+"""
+
 BLUE = "\033[94m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
